4fc939ea793df8d69b90ad59f0482c91a3b26e7b
Xavier Juste <xavier.juste@ing.com>
2019-01-22 12:28:29 +0100

    Initial push

---
+++ src/main/java/com/toutjuste/sccpoc/SccpocApplication.java
@@ -0,0 +1,14 @@
+package com.toutjuste.sccpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccpocApplication.class, args);
+	}
+
+}
+
